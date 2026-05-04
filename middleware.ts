b51895f07435f5cf26d264e42f9439af92e58de6
Filatov Anton <positron48@gmail.com>
2026-05-04 16:31:27 +0300

    favicon

--- middleware.ts
+++ middleware.ts
@@ -38,5 +38,7 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+  matcher: [
+    "/((?!_next/static|_next/image|favicon\\.ico|icon\\.svg|apple-icon\\.png).*)",
+  ],
 };
